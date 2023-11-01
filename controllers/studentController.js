b53b04f20050d344f94dc74d660b4a931e816a11
Parjanya Shakya <parjanya57@gmail.com>
2023-11-01 17:22:58 +0545

    Sinon JS implementation and Spy function

---
+++ controllers/studentController.js
@@ -0,0 +1,21 @@
+class StudentController {
+    constructor() {
+
+    }
+
+    home(type) {
+        let data = this.getInfo(type,1);
+        // this.getInfo(type,1);
+        return data + 5;
+    }
+
+    userId() {
+        return 12;
+    }
+
+    getInfo(type,status) {
+        return type;
+    }
+}
+
+module.exports = StudentController
--- test-case/spay.spec.js
+++ test-case/spay.spec.js
@@ -2,4 +2,32 @@ var sinon = require('sinon')
 
 var chai = require('chai')
 
-// var expect = chai.expect;
+var expect = chai.expect;
+var should = chai.should();
+var assert = chai.assert;
+
+var student = require('../controllers/studentController');
+var studentObj = new student();
+
+describe('Spay',function(){
+    
+    it('test userInfo Function',() => {
+        expect(studentObj.userId()).to.be.equal(12);
+        // studentObj.userId().should.be.equal(12) //Works
+        // assert.equal(studentObj.userId(),12) //Works
+    })
+    
+    // it('function count',() => {
+    //     var spyObj = sinon.spy(studentObj,'getInfo');
+    //     studentObj.home(5);
+    //     expect(spyObj.calledOnce).to.be.true; //calledOne returns true if function is called once.
+    //     // expect(spyObj.calledTwice).to.be.true; // calledTwice returns true if function is called twice.
+    // })
+
+    it('function arguments check', () => {
+        var spyObj = sinon.spy(studentObj,'getInfo');
+        studentObj.home(5);
+        expect(spyObj.calledWith(5,1)).to.be.true; // calledWith returns true if the same parameters are passed to the spied function (must be exact)
+    })
+    
+})
